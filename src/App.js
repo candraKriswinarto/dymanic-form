e8020cb6d954460d7e79955bd892f3cd82722e85
candra kriswinarto <ckriswinarto@gmail.com>
2020-07-20 06:36:14 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  button: {
+    margin: theme.spacing(1),
+  }
+}))
 
 function App() {
+  const classes = useStyles()
+  const [inputFields, setInputFields] = useState([
+    { firstName: '', lastName: '' },
+    { firstName: '', lastName: '' },
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("InputFields", inputFields);
+  };
+
+  const handleChangeInput = (index, event) => {
+    const values = [...inputFields];
+    values[index][event.target.name] = event.target.value;
+    setInputFields(values);
+  }
+
+  const handleAddFields = () => {
+    setInputFields([...inputFields, { firstName: '', lastName: '' }])
+  }
+
+  const handleRemoveFields = (index) => {
+    const values  = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Add New Member</h1>
+      <form className={classes.root} onSubmit={handleSubmit}>
+        { inputFields.map((inputField, index) => (
+          <div key={index}>
+            <TextField 
+              name="firstName"
+              label="First Name"
+              variant="filled"
+              value={inputField.firstName}
+              onChange={event => handleChangeInput(index, event)}
+            />
+            <TextField 
+              name="lastName"
+              label="Last Name"
+              variant="filled"
+              value={inputField.lastName}
+              onChange={event => handleChangeInput(index, event)}
+            />
+            <IconButton
+              onClick={() => handleRemoveFields(index)}
+            >
+              <RemoveIcon />
+            </IconButton>
+            <IconButton
+              onClick={() => handleAddFields()}
+            >
+              <AddIcon />
+            </IconButton>
+          </div>
+        )) }
+        <Button
+          className={classes.button}
+          variant="contained" 
+          color="primary" 
+          type="submit" 
+          endIcon={<Icon>send</Icon>}
+          onClick={handleSubmit}
+        >Send</Button>
+      </form>
+    </Container>
   );
 }
 
